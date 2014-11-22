5d16abc72e7ad7f216df180c02ce9a2d38d3bdc7
Cameron Little <cameron@camlittle.com>
2014-11-22 12:15:33 -0800

    Initial Commit

---
+++ Photos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Photos
+//
+//  Created by Cameron Little on 11/22/14.
+//  Copyright (c) 2014 Cameron Little. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Photos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by Cameron Little on 11/22/14.
+//  Copyright (c) 2014 Cameron Little. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
