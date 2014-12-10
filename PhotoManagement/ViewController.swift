248f0f5c40fbbe2378ae7276b9054fb444a4a5bb
Cameron Little <cameron@camlittle.com>
2014-12-09 18:57:32 -0800

    Initial Commit

---
+++ PhotoManagement/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PhotoManagement
+//
+//  Created by Cameron Little on 12/9/14.
+//  Copyright (c) 2014 Cameron Little. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
